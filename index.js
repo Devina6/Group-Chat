9b7acca30ea2e7163df056d66e3029da24235ebf
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-27 16:14:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use((req,res,next)=>{
+	res.status(404).send('<h1>Page not found</h1>');
+})
+
+app.listen(4000);
